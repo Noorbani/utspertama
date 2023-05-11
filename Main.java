aecf326f75ec8022b81aa63faba623b2078e9235
Noorbani <=noorbaniadam23@gmail.com>
2023-05-11 16:40:32 +0700

    UTS

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 class Main {
     public static void main(String[] args) {
-//INI KOMENTAR
+
+        Mhs mhs = new mhs(username:"ini_username", password:"ini_password");
+
+        mhs.setWarna(nama:"noor bani adam");
+        mhs.setNim(nim:"g111190025");
+        mhs.setSmt(smt:8);
+
+        System.out.println("Nama Mhs: " +  mhs.getNama());
+        System.out.println("Nim Mhs: "  + mhs.getNim());
+        System.out.println("Smt Mhs: " + mhs.getSmt());
+        System.out.println("Username: " + mhs.getUsername());
+        System.out.println("password: " + mhs.getpassword());
+
+
     }
 }
--- pengguna/Mhs.java
+++ pengguna/Mhs.java
@@ -1,7 +1,41 @@
 package pengguna;
 
-class Mhs {
-    Mhs () {
-
-    }
+public class Mhs {
+    private String nama;
+    private String nim;
+    private int semester;
+    
+   public Mhs (String username, String password, String nama, String nim, int semester) {
+    
+    this.nama = nama;
+    this.nim = nim;
+    this.semester = semester;
 }
+
+public void setNama(String nama) {
+    this.nama = nama;
+}
+
+public String getNama() {
+    return this.nama;
+}
+
+public void setNim(String nim) {
+    this.nim = nim;
+}
+
+public String getNim() {
+    return this.nim;
+}
+
+public void setSemester(int semester) {
+    this.semester = semester;
+}
+
+public int getSemester() {
+    return this.semester;
+}
+}
+
+    
+
